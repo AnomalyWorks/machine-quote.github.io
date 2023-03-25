340c9945bf0ac71b9392c1ee70d8cdf0d0694230
Abel <121730691+AnomalyWorks@users.noreply.github.com>
2023-03-25 03:13:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 const COLORS= ["#2f2bad","#ad2bad","#e42692","#f7db15","#5c65c0","#8c7c62"];
+const TIME_QUOTE= 800;
 
 $(document).ready(() => {
     textFadeOut();
@@ -30,15 +31,15 @@ function setQuote(){
 }
 
 function textFadeOut(){
-    $("#text, #author").animate({opacity: 0},800).promise().then(setQuote);
+    $("#text, #author").animate({opacity: 0},TIME_QUOTE).promise().then(setQuote);
 }
 
 function textFadeIn(){
-    $("#text, #author").animate({opacity: 1},800);
+    $("#text, #author").animate({opacity: 1},TIME_QUOTE);
 }
 
 function setNewColor(){
     let colorS= COLORS[Math.floor(Math.random() * COLORS.length)];
-    $("#text, #author").animate({color: colorS}, 800);
-    $(".all-button, body").animate({backgroundColor: colorS}, 800);
+    $("#text, #author").animate({color: colorS}, TIME_QUOTE);
+    $(".all-button, body").animate({backgroundColor: colorS}, TIME_QUOTE);
 }
